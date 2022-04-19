4d11b4dd53d1e7d1c3001afb8fe9fb5cefe8ca97
sena <senagtms1@gmail.com>
2022-04-19 12:55:43 +0300

    Update cihazlar_router.js

--- router/cihazlar_router.js
+++ router/cihazlar_router.js
@@ -3,9 +3,11 @@ const cihazController =require('../controller/cihazlar_controller');
 
 const cihazlar_controller = express.Router()
 
-cihazlar_controller.get('/',cihazController.getAll);
-cihazlar_controller.get('/cihazEkle', cihazController.ekleForm); 
-cihazlar_controller.post('/cihazEkle', cihazController.getCreate);
-// cihazlar_controller.get('/:id',cihazController.getCihazdetay)
+cihazlar_router.get('/',cihazController.getAll);
+cihazlar_router.get('/cihazEkle', cihazController.ekleForm); 
+cihazlar_router.post('/cihazEkle', cihazController.getCreate);
+cihazlar_router.get('/cihazdetay/:id',cihazController.getCihazdetay);
+cihazlar_router.get('/:id',cihazController.delete);
+
 
 module.exports=cihazlar_controller;

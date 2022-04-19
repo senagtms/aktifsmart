afbfa95641847d7ae06bd651662007b860b75144
sena <senagtms1@gmail.com>
2022-04-19 12:55:00 +0300

    Update sensorler_router.js

--- router/sensorler_router.js
+++ router/sensorler_router.js
@@ -2,13 +2,10 @@ const express =require("express");
 const sensorController =require('../controller/sensorler_controller');
 const sensorler_controller = express.Router()
 
-sensorler_controller.get('/sensorEkle',sensorController.getAll);
-sensorler_controller.get('/sensorEkle',sensorController.getById);
-sensorler_controller.post('/sensorEkle', sensorController.getCreate);
-sensorler_controller.get('/:id',sensorController.getCihazdetay)
-// sensorler_controller.get('/cihazlar/:id',sensorController.getCihazdetay)
-sensorler_controller.post('/sensorUpdate', sensorController.sensorUpdate);
-// sensorler_controller.get('/sensordetay/sensorEkle',sensorController.sensorEkle);
-// sensorler_controller.get('/sensordetay/alarm',sensorController.alarm)
+sensorler_controller.get('/sensorEkle/:id',sensorController.getAll);
+sensorler_controller.get('/sensorEkle/:id',sensorController.getById);
+sensorler_controller.post('/sensorEkle/:id', sensorController.getCreate);
+
+
 
 module.exports=sensorler_controller;

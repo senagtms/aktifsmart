8d35d3ed204ddce40a7718eafec4a4181f92b8ab
sena <senagtms1@gmail.com>
2022-04-19 12:53:14 +0300

    Update sensorler_model.js

--- models/sensorler_model.js
+++ models/sensorler_model.js
@@ -1,38 +1,37 @@
 const mysql = require("../db/database");
-
 const Sensorler = function (vt_sensorler) {
-    this.sensorTipID  = vt_sensorler.sensorTipID  ;
+    this.sensorTipID  = vt_sensorler.sensorTipID ;
     this.sensorKodu = vt_sensorler.	sensorKodu;
     this.sensorAdi=vt_sensorler.sensorAdi;
     this.sensorDurumu=vt_sensorler.sensorDurumu;
   };
-  
 
-  Sensorler.create = function (yenisensor, result) {
+  Sensorler.create = function (id,yenisensor, result) {
   
-    mysql.query("INSERT INTO vt_sensorler SET ?", yenisensor, function (err, res) {
+    mysql.query(`INSERT INTO vt_sensorler (sensorTipID,sensorKodu,sensorAdi,sensorDurumu, SensorCihazID) VALUES ('${yenisensor.sensorTipID}','${yenisensor.sensorKodu}','${yenisensor.sensorAdi}',${yenisensor.sensorDurumu} ,${id})`, function (err, res) {
       if (err) {
         console.log("error: ", err);
         result(err, null);
         return;
-      }
-  
+      } 
       console.log("Sensor oluşturuldu: ", { id: res.insertId, ...yenisensor });
       result(null, { id: res.insertId, ...yenisensor });
     });
+
   };
 
-  Sensorler.updatee = function(sensorTipID, sensordurum, result){
-    // mysql.query('UPDATE vt_sensorler SET sensorDurumu =  $sensorDurumu WHERE sensorTipID = $sensorTipID', [sensordurum.sensorDurumu, sensorTipID], function (err, res) {
-    //         if(err) {
-    //             console.log("error: ", err);
-    //               result(null, err);
-    //            }
-    //          else{   
-    //            result(null, res);
-    //               }
-    //           }); 
-    console.log(sensorTipID+sensordurum);
-  };
   
-module.exports = Sensorler;
+  Sensorler.getCihaz = function (id,result) {
+    mysql.query(`Select * from vt_sensorler where sensorID =?`,id,function (err, res) {             
+      if(err) {
+          console.log("error: ", err);
+          result(err, null);
+      }
+      else{
+          result(null, res);
+      }
+  });  
+  };
+
+
+

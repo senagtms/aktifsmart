9e4389eab49cf1e9733c83bf210a2379d7032ccd
sena <senagtms1@gmail.com>
2022-04-22 16:53:46 +0300

    Update sensorler_router.js

--- router/sensorler_router.js
+++ router/sensorler_router.js
@@ -7,5 +7,13 @@ sensorler_controller.get('/sensorEkle/:id',sensorController.getById);
 sensorler_controller.post('/sensorEkle/:id', sensorController.getCreate);
 
 
+sensorler_controller.get('/cihazdetay/alarm/:id',sensorController.alarmGetAll);
+sensorler_controller.get('/cihazdetay/nem/:id',sensorController.nemGetAll);
+sensorler_controller.get('/cihazdetay/sicaklik/:id',sensorController.sicaklikGetAll);
+sensorler_controller.get('/cihazdetay/yaniciGaz/:id',sensorController.yaniciGazGetAll);
+sensorler_controller.get('/cihazdetay/siren/:id',sensorController.sirenGetAll);
+sensorler_controller.get('/cihazdetay/gaz/:id',sensorController.gazGetAll);
+sensorler_controller.get('/cihazdetay/suBaskini/:id',sensorController.suBaskiniGetAll);
+
 
 module.exports=sensorler_controller;

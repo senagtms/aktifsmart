9443979430fd7900a00da975007de680d72adbc2
sena <senagtms1@gmail.com>
2022-05-23 09:40:25 +0300

    Update cihazlar_model.js

--- models/cihazlar_model.js
+++ models/cihazlar_model.js
@@ -1,3 +1,4 @@
+
 const mysql = require("../db/database");
 
 const Cihazlar = function (vt_cihaz) {
@@ -9,9 +10,17 @@ const Cihazlar = function (vt_cihaz) {
     this.cihazKodu = vt_cihaz.cihazKodu;
     this.cihazAdresi = vt_cihaz.cihazAdresi;
     this.cihazDurumu = vt_cihaz.cihazDurumu;  
+
+
+  /*cihaza ait kullanıcılar için */
+    this.kullaniciCihazID=vt_cihaz.kullaniciCihazID;
+    this.kullaniciID=vt_cihaz.kullaniciID;
+    this.kullaniciAdi=vt_cihaz.kullaniciAdi;
+    this.kullaniciSoyadi=vt_cihaz.kullaniciSoyadi;
+
   };
   
-  Cihazlar.findAll = function (result) {
+  Cihazlar.veriListele = function (result) {
     const sql = "SELECT * FROM vt_cihaz";
     mysql.query(sql, (err, data) => {
       if (err) {
@@ -24,7 +33,7 @@ const Cihazlar = function (vt_cihaz) {
     });   
   };
    
-  Cihazlar.create = function (yenicihaz, result) {
+  Cihazlar.olustur = function (yenicihaz, result) {
     mysql.query("INSERT INTO vt_cihaz SET ?", yenicihaz, function (err, res) {
       if (err) {
         console.log("error: ", err);
@@ -36,19 +45,49 @@ const Cihazlar = function (vt_cihaz) {
       result(null, { id: res.insertId, ...yenicihaz });
     });
   };
-
-  Cihazlar.getCihaz = function (selectedCihaz) {
-    const sql = `SELECT * FROM vt_cihaz WHERE cihazID =${selectedCihaz}`;
-    mysql.query(sql, (err, data) => {
-      if (err) {
-        console.log("error: ", err);
-        return err;
-      } else {
-        console.log("Cihazlar : ", data);
-        return data;
+  
+  Cihazlar.cihazDetay = function (id,result) {
+    mysql.query(`Select * from vt_cihaz where cihazID =?`,id,function (err, res) {             
+      if(err) {
+          console.log("error: ", err);
+          result(err, null);
       }
-    });   
+      else{
+          result(null, res);
+      }
+  });  
   };
- 
+  Cihazlar.cihazSil = function(id, result){
+    mysql.query("DELETE FROM vt_cihaz WHERE cihazID = ?", [id], function (err, res) {
+  
+               if(err) {
+                   console.log("error: ", err);
+                   result(null, err);
+               }
+               else{
+                  console.log("Delete Successfully!!!");
+                result(null, res);
+               }
+           }); 
+  };
+
+/***************** Cihaza ait kullanıcılar bölümü *********************/
+
+
+
+Cihazlar.cihazKullanicilar = function (id,result) {
+  mysql.query(`Select * from vt_kullaniciCihaz where cihazID =?`,id,function (err, res) {             
+    if(err) {
+        console.log("error: ", err);
+        result(err, null);
+    }
+    else{
+        result(null, res);
+    }
+});  
+};
+
+
+
   
   module.exports = Cihazlar;
